8670cb08ddd6374b46361fc0d5fe8be7269c5087
Dongnyeok Shin <dn.shin98@gmail.com>
2021-05-02 17:47:49 +0900

    [study] spring_boot CRUD

---
+++ Spring_boot/board/src/test/java/com/example/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.board;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
